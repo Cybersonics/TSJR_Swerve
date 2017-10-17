7838da1a13db84e4652c6daa4073a3e6b30d6d2b
1002089 <1002089@HS-219-06.pasd.palisadessd.org>
2017-10-17 18:34:16 -0400

    Added WHEEL_BASE values.

--- TSJR_Swerve/src/org/usfirst/frc/team103/robot/Drive.java
+++ TSJR_Swerve/src/org/usfirst/frc/team103/robot/Drive.java
@@ -9,7 +9,16 @@ import com.ctre.CANTalon;
  */
 public class Drive extends Subsystem {
 	
+	//units for WHEEL_BASE in inches
 	
+	public static final double WHEEL_BASE_LENGTH = 27.0;
+	public static final double WHEEL_BASE_WIDTH = 22.0;
+	
+	public void swerveDrive(double strafe, double forward, double omega) {
+		
+		
+		
+	}
 	
 	@Override
 	protected void initDefaultCommand() {
